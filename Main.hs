e50d9f7128ab622ef9e3ef0a439f61df117896cb
winterland1989 <winterland1989@gmail.com>
2016-07-14 18:37:40 +0800

    test escape

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import qualified Data.Aeson as AE
+import qualified Data.Aeson.Parser as AE
+import qualified Data.Attoparsec.ByteString as AT
 import qualified Data.JsonStream.Parser as JS
-import Data.Aeson.TH
 import qualified Data.ByteString as B
 import Control.Monad (replicateM_)
 import Criterion.Main
@@ -12,12 +12,12 @@ import Criterion.Main
 
 main :: IO ()
 main = defaultMain
-    [ env (B.readFile "./twitter10.json") $ \ t -> bgroup "twitter10.json" (go t)
-    , env (B.readFile "./twitter100.json") $ \ t -> bgroup "twitter100.json" (go t)
+    [ env (B.readFile "./escape.txt") $ \ t -> bgroup "escape.json" (go t)
+    , env (B.readFile "./noescape.txt") $ \ t -> bgroup "noescape.json" (go t)
     ]
 
 go :: B.ByteString -> [Benchmark]
 go bs =
-    [ bench "aeson parser"       $ nf (AE.decodeStrict :: B.ByteString -> Maybe AE.Value) bs
-    , bench "json-stream parser" $ nf (JS.decodeStrict :: B.ByteString -> Maybe AE.Value) bs
+    [ bench "aeson parser"       $ nf (AT.parse AE.jstring) bs
+    , bench "json-stream parser" $ nf (JS.parseByteString JS.string) bs
     ]
